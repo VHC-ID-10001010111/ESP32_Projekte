a1cfa8ee20717180d180733328903f793167a9f8
VHC-ID-10001010111 <klemm.konstantin@web.de>
2026-08-27 11:22:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print("=== OTA TEST erfolgreich! ===")
-print("Diese Version wurde am", "27.08.2026", "von GitHub geladen.")
-print("Wenn du das hier siehst, funktioniert Hash-Erkennung + Syntaxpruefung.")
+import anzeige
+anzeige.log("=== OTA TEST ===")
+anzeige.log("Version vom 27.08.")
+anzeige.log("Alles läuft stabil.")
